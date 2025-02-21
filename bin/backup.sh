f9fbaabf30f68b5206925e3ee3fdc8d1159e5480
Evgeny Feigelman <devops.evgeny@gmail.com>
2025-02-22 00:48:56 +0200

    modified:   backup.sh

---
+++ bin/backup.sh
@@ -0,0 +1,15 @@
+
+#!/usr/bin/env bash
+# -----------------------------------------------------
+# Script Name:    my_script.sh
+# Version:        1.24.0
+# Author:         Feigelman Evgeny
+# Date:           2025-02-12
+# Description:    This script will install most of needed packages, that you will use
+#                 during your dayly work as a DevOps expert. Fill free to tell me if
+#                 you think that this script can be improved.
+set -o errexit
+set -o pipefail
+set -x
+# -----------------------------------------------------
+

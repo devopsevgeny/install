2728fb7b18fc0ee7a9b50596c60b16912b7f1f6d
Evgeny Feigelman <devops.evgeny@gmail.com>
2025-02-22 21:17:28 +0200

    modified:   vim_plugins.sh

--- bin/vim_plugins.sh
+++ bin/vim_plugins.sh
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env bash
 # -----------------------------------------------------
 # Script Name:    my_script.sh
@@ -11,5 +10,58 @@
 set -o errexit
 set -o pipefail
 set -x
+vimrcfile=$HOME/.vimrc
+vimrcderectory=$HOME/.vim
+
 # -----------------------------------------------------
+
+# Update vimrc file 
+create_vimrc(){
+    if [[ ! -f  $vimrcfile ]]; then
+        curl -o $vimrcfile  "https://raw.githubusercontent.com/devopsevgeny/install/refs/heads/main/.vimrc"
+    else
+        cp "$vimrcfile" "${vimrcfile}.old"
+        echo "Created a backup of vimrc"
+        curl -o $vimrcfile  "https://raw.githubusercontent.com/devopsevgeny/install/refs/heads/main/.vimrc"
+    fi
+}
+
+# Add more color schemes to vim.
+add_color_schemes(){
+    if [[ ! -d $vimrcderectory ]];then
+        mkdir -p $vimrcderectory/pack/colors/start
+        echo "Derectory .vimrc has been created, downloading color scemes"
+        cd $vimrcderectory/pack/colors/start
+        git clone https://github.com/rafi/awesome-vim-colorschemes.git
+        git clone https://github.com/flazz/vim-colorschemes.git
+    else
+        echo "Already exist"
+    fi
+}
+
+# Install vim-plugins
+
+install_plugins(){
+    if [[ ! -f $vimrcderectory/autoload/plug.vim ]]; then
+        curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+        # Install plugins
+        echo "Installing Vim plugins..."
+        vim +PlugInstall +qall
+        if [[ !? -eq 0 ]];then
+            echo "Vim plugins installed successfully!"
+        else
+            "Failed to install"
+        fi
+    else
+        echo "Already installed"
+    fi
+
+    }
+
+
+
+create_vimrc
+add_color_schemes
+install_plugins
 
